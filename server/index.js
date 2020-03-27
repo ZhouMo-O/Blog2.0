bdbecec33eb8cda84de1f0504175a1bf8b22c617
xiaowu <1181649328@qq.com>
2020-03-27 14:20:07 +0800

    增加注释

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = 5555;
 
 //-------------------------中间件--------------------------------
 require("./plugin/db")(app); //db
-require("./router/router")(app);
+require("./router/router")(app); //router
 
 app.listen(PORT, () => {
   console.log(`服务启动 端口号:${PORT}`);
