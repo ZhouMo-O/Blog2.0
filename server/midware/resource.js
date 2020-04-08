7b106ec6ce3b75ff4676d98422e00fbd677733e5
five <1181649328@qq.com>
2020-04-08 23:07:30 +0800

    开发通用CURD接口

---
+++ server/midware/resource.js
@@ -0,0 +1,6 @@
+module.exports = (resource) => async (req, res, next) => {
+  console.log(`当前访问的表:`, req.params.resource);
+  const nodeName = require("inflection").classify(req.params.resource);
+  req.Model = require(`../model/${nodeName}`);
+  next();
+};
--- server/router/router.js
+++ server/router/router.js
@@ -1,10 +1,43 @@
-module.exports = app => {
+module.exports = (app) => {
   const express = require("express");
   const router = express.Router({ mergeParams: true });
+  const resourceMiddleware = require("../midware/resource");
 
-  router.get("/", (req, res) => {
-    res.send("hi");
+  router.post("/", async (req, res) => {
+    const model = await req.Model.create(req.body);
+    console.log(`创建数据`, req.body);
+    console.log(model);
+    res.send(model);
   });
 
-  app.use("/", router);
+  router.get("/", async (req, res) => {
+    const item = await req.Model.find();
+    console.log(`获取 ${req.params.resource}列表`);
+    res.send(item);
+  });
+
+  router.get("/:id", async (req, res) => {
+    const item = await req.Model.findById(req.params.id).populate(
+      "relatedProject"
+    );
+    console.log(`查找 ${req.params.id}`);
+    console.log(item);
+    res.send(item);
+  });
+
+  router.put("/:id", async (req, res) => {
+    const item = await req.Model.findByIdAndUpdate(req.params.id, req.body);
+    console.log(`查找 ${req.params.id}`);
+    console.log(item);
+    res.send(item);
+  });
+
+  router.delete("/:id", async (req, res) => {
+    console.log(req.params.id);
+    const data = await req.Model.findByIdAndDelete(req.params.id);
+    console.log(`删除 ${req.params.resource} 中的 ${data}`);
+    res.send(data);
+  });
+
+  app.use("/api/rest/:resource", resourceMiddleware(), router);
 };
