82e84195e702769517a70a841f75b9722a3e89ee
five <1181649328@qq.com>
2020-04-08 22:04:03 +0800

    后端管理界面添加markdown编辑器

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import main from "../views/main.vue";
 import home from "../views/home.vue";
-import article from "../views/article/articleCreate";
+import articleCreate from "../views/article/articleCreate";
 
 Vue.use(VueRouter);
 
@@ -10,17 +10,22 @@ const router = new VueRouter({
   routes: [
     {
       path: "/",
-      name: "home",
+      name: "main",
       component: main,
       children: [
         {
           path: "/home",
           name: home,
-          component: home
-        }
-      ]
-    }
-  ]
+          component: home,
+        },
+        {
+          path: "/article/create",
+          name: articleCreate,
+          component: articleCreate,
+        },
+      ],
+    },
+  ],
 });
 
 export default router;
---
+++ admin/src/views/article/articleCreate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="editor">
+    <mavon-editor :ishljs="true" style="height: 100%"></mavon-editor>
+  </div>
+</template>
+
+<script>
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    mavonEditor,
+  },
+};
+</script>
+
+<style>
+#editor {
+  margin: auto;
+  width: 90%;
+  height: 580px;
+}
+</style>
