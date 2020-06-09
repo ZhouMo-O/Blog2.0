82766715dfe7b9f68d064fd05ae0f4597a4082eb
five <1181649328@qq.com>
2020-06-09 22:39:29 +0800

    设计首页博客列表的 点赞 评论 和查看的ui

--- web/components/articleCard.vue
+++ web/components/articleCard.vue
@@ -8,39 +8,60 @@
         v-for="item in model"
         :key="item._id"
         id="articleCard"
-        ><nuxt-link :to="'/article/article'">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-card
-                :elevation="hover ? 2 : 10"
-                class="mx-auto"
-                max-width="350"
-                max-height="380"
+      >
+        <v-hover>
+          <template v-slot="{ hover }">
+            <v-card
+              :elevation="hover ? 2 : 10"
+              class="mx-auto"
+              max-width="350"
+              max-height="380"
+              :to="'/article/article'"
+            >
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
               >
-                <v-img
-                  class="white--text align-end"
-                  height="200px"
-                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                >
-                  <v-card-title>Top 10 Australian beaches</v-card-title>
-                </v-img>
+                <v-card-title>{{ item.title }}</v-card-title>
+              </v-img>
 
-                <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+              <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
 
-                <v-card-text class="text--primary">
-                  <div>Whitehaven Beach</div>
+              <v-card-text class="text--primary">
+                <div>Whitehaven Beach</div>
 
-                  <div>Whitsunday Island, Whitsunday Islands</div>
-                </v-card-text>
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
 
-                <v-card-actions>
-                  <v-btn color="orange" text>Share</v-btn>
-                  <v-btn color="orange" text>Explore</v-btn>
-                </v-card-actions>
-              </v-card>
-            </template>
-          </v-hover>
-        </nuxt-link>
+              <v-card-actions class="articleMessage">
+                <!-- time -->
+                <span class="overline">
+                  {{ item.createTime.slice(0, 8) }}
+                </span>
+                <div class="likeAndRead">
+                  <!-- read -->
+                  <span class="read overline">
+                    <v-icon class="articleIcon">mdi-eye-outline</v-icon>
+                    {{ item.read }}
+                  </span>
+                  <!-- comment -->
+                  <span class="comment overline">
+                    <v-icon class="articleIcon"
+                      >mdi-comment-processing-outline</v-icon
+                    >
+                    10
+                  </span>
+                  <!-- like -->
+                  <span class="like overline">
+                    <v-icon class="articleIcon">mdi-thumb-up-outline</v-icon>
+                    {{ item.like }}
+                  </span>
+                </div>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
     </v-row>
   </v-container>
@@ -67,6 +88,19 @@ export default {
 </script>
 
 <style scoped>
+.articleMessage {
+  position: relative;
+}
+
+.likeAndRead {
+  position: absolute;
+  right: 10px;
+}
+
+.articleIcon {
+  font-size: 20px;
+  margin-right: 2px;
+}
 @media screen and (min-width: 1500px) {
   #cardIndex {
     max-width: 1450px;
