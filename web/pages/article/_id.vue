ee1433e399485a6523ecfb0876ba8bdfac640be8
five <1181649328@qq.com>
2020-06-21 22:27:17 +0800

    开发评论功能前端展示

--- web/pages/article/_id.vue
+++ web/pages/article/_id.vue
@@ -76,6 +76,35 @@
           ></v-textarea>
           <v-btn :disabled="!valid" @click="submit" clos="md-12">submit</v-btn>
         </v-form>
+        <v-divider class="mt-4 mb-4"></v-divider>
+        <v-card :tile="false" class="mx-auto" color="#26c6da" max-width="100%">
+          <v-card-actions>
+            <v-list-item class="grow">
+              <v-list-item-avatar color="grey darken-3">
+                <v-img
+                  class="elevation-6"
+                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                ></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>Evan You</v-list-item-title>
+              </v-list-item-content>
+
+              <v-row align="center" justify="end">
+                <v-icon class="mr-1">mdi-heart</v-icon>
+                <span class="subheading mr-2">256</span>
+                <span class="mr-1">·</span>
+                <v-icon class="mr-1">mdi-share-variant</v-icon>
+                <span class="subheading">45</span>
+              </v-row>
+            </v-list-item>
+          </v-card-actions>
+          <v-card-text class="text-h3">
+            "Turns out semicolon-less style is easier and safer in TS because
+            most gotcha edge cases are type invalid as well."
+          </v-card-text>
+        </v-card>
       </v-card>
     </v-container>
   </div>
@@ -118,7 +147,8 @@ export default {
       this.updateArticleInfo();
     },
     submit() {
-      this.$refs.form.validate();
+      // this.$refs.form.validate();
+      console.log(this.comment);
     }
   },
   components: {},
