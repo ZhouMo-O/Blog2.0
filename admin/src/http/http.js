77bfd76d5bea223c668c98090a2768a8289c9b1a
five <1181649328@qq.com>
2020-07-16 23:41:42 +0800

    在http请求中添加token

--- admin/src/http/http.js
+++ admin/src/http/http.js
@@ -4,30 +4,30 @@ import router from "../router";
 
 const http = axios.create({
   baseURL: process.env.VUE_APP_API || "/api",
-  withCredentials: true
+  withCredentials: true,
 });
 //顺序很重要必须要先在请求头中加上token
 http.interceptors.request.use(
-  config => {
+  (config) => {
     if (localStorage.token) {
-      // config.headers.Authorization = "Bearer " + localStorage.token;
+      config.headers.Authorization = "Bearer " + localStorage.token;
     }
     return config;
   },
-  err => {
+  (err) => {
     return Promise.reject(err);
   }
 );
 
 http.interceptors.response.use(
-  res => {
+  (res) => {
     return res;
   },
-  err => {
+  (err) => {
     if (err.response.data.message) {
       Vue.prototype.$message({
         type: "error",
-        message: err.response.data.message
+        message: err.response.data.message,
       });
       if (err.response.status === 401) {
         router.push("/login");
