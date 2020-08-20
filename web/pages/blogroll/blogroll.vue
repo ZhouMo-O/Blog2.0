e16de3f5ca2c044a12a09bdedbeb6a0da4c53c95
five <1181649328@qq.com>
2020-08-20 22:58:31 +0800

    优化友情链接样式

--- web/pages/blogroll/blogroll.vue
+++ web/pages/blogroll/blogroll.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>友情链接</h1>
+  <div>
+    <v-card color="#F0F0F0" class="articleTopCard">
+      <h2 class="text-center">友情链接 , 链接友情</h2>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.articleTopCard {
+  width: 100vw;
+  height: 45vh;
+  background: #4f7da4;
+  /* border: 1px solid red !important; */
+  background-size: cover;
+}
+
+h2 {
+  text-align: center;
+  padding-top: 15vh;
+}
+</style>
