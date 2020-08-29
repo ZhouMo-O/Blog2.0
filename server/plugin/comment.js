3c3a39e0723f6d1b9434f71abba7c2f189a67501
five <1181649328@qq.com>
2020-08-29 12:37:29 +0800

    在路由中增加博主回复校验

--- server/plugin/comment.js
+++ server/plugin/comment.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   const sendEmail = require("./sendEmail");
 
   app.post("/api/comment", async (req, res) => {
-    if (!req.body.istopComment) {
+    if (req.body.istopComment) {
       const model = await commentDb.create(req.body);
       await articleDb.findByIdAndUpdate(
         { _id: req.body.blogId },
@@ -13,7 +13,8 @@ module.exports = (app) => {
       sendEmail(process.env.eamailUser, `收到一条评论：${req.body.content}`);
       console.log(`评论数据`, req.body);
       res.send(model);
-    } else {
+    } else if (req.body.isauthorsComment) {
+      console.log("博主回复");
       const model = await commentDb.create(req.body);
       await articleDb.findByIdAndUpdate(
         { _id: req.body.blogId },
@@ -24,8 +25,28 @@ module.exports = (app) => {
       if (replyEmail.email) {
         sendEmail(replyEmail.email, `收到一条回复：${req.body.content}`);
       }
-      sendEmail(process.env.eamailUser, `收到一条评论：${req.body.content}`);
-      console.log(`评论数据`, req.body);
+      // sendEmail(
+      //   process.env.eamailUser,
+      //   `一条评论收到回复：${req.body.content}`
+      // );
+      console.log(`评论数据(回复)：`, req.body);
+      res.send(model);
+    } else {
+      const model = await commentDb.create(req.body);
+      await articleDb.findByIdAndUpdate(
+        { _id: req.body.blogId },
+        { $inc: { comment: 1 } }
+      );
+      const replyEmail = await commentDb.findById(req.body.replyId);
+      console.log(`查找id`, replyEmail);
+      if (replyEmail.email) {
+        // sendEmail(replyEmail.email, `收到一条回复：${req.body.content}`);
+      }
+      // sendEmail(
+      //   process.env.eamailUser,
+      //   `一条评论收到回复：${req.body.content}`
+      // );
+      console.log(`评论数据(回复)：`, req.body);
       res.send(model);
     }
   });
