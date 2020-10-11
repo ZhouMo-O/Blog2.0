52d388f3e0684b46001be301badc510f5276d460
five <1181649328@qq.com>
2020-10-11 19:56:22 +0800

    增加首次访问的欢迎标语

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <!-- <v-card>111 <v-overlay :absolute="absolute"> </v-overlay></v-card> -->
-    <v-card class="indexCard"> </v-card>
+    <v-card>
+      <v-img
+        class="indexCard"
+        src="https://xiaowublog1.oss-cn-shanghai.aliyuncs.com/starry.jpg"
+        aspect-ratio="1.7"
+      >
+        <transition name="fade">
+          <div v-if="show" class="greet">
+            <h1>周末的个人博客</h1>
+            <h5 class="">脚踏实地，仰望星空</h5>
+          </div>
+        </transition></v-img
+      >
+    </v-card>
     <articleCard></articleCard>
   </div>
 </template>
@@ -11,22 +24,42 @@ import Logo from "~/components/Logo.vue";
 import articleCard from "~/components/articleCard.vue";
 export default {
   data() {
-    return {};
+    return {
+      show: false
+    };
   },
   mmethods: {},
   components: {
     Logo,
     articleCard
+  },
+  mounted() {
+    this.show = true;
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .indexCard {
-  background: url("https://xiaowublog1.oss-cn-shanghai.aliyuncs.com/starry.jpg");
+  $border-color: #f90;
+  // background: url("https://xiaowublog1.oss-cn-shanghai.aliyuncs.com/starry.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   width: 100vw;
   height: 100vh;
+  color: white;
+  .greet {
+    // border: 1px solid $border-color;
+    margin: 0 auto;
+    text-align: center;
+    padding: 35vh 0;
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
